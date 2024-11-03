d2c21ce98ca58b4ff177fe14742c98a5ce4116aa
John Veresk <john.veresk@proton.me>
2024-11-03 12:15:54 +0200

    refactor Chat

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -9,69 +9,98 @@ interface Props {
 
 export function Chat({ swarm, createTopic }: Props) {
   const [topic, setTopic] = useState<string | null>(null);
-  const [message, setMessage] = useState("");
-  const [showTopicFrom, setShowTopicForm] = useState(false);
+
+  return (
+    <div>
+      {!topic ? (
+        <StartPanel
+          onTopic={async (topic) => {
+            await swarm.join(topic);
+            setTopic(topic);
+          }}
+          createTopic={createTopic}
+        />
+      ) : (
+        <>
+          <p>{topic}</p>
+          <MessageEditor onSubmit={(message) => swarm.sendAll(message)} />
+        </>
+      )}
+    </div>
+  );
+}
+
+function StartPanel({
+  onTopic,
+  createTopic,
+}: {
+  onTopic: (topic: string) => void;
+  createTopic: () => string;
+}) {
+  const [showTopicForm, setShowTopicForm] = useState(false);
+
+  return (
+    <div>
+      <button
+        onClick={async () => {
+          onTopic(createTopic());
+        }}
+      >
+        Start Chat
+      </button>
+      <button onClick={() => setShowTopicForm(true)}>Join Chat</button>
+      {showTopicForm && <TopicEditor onSubmit={onTopic} />}
+    </div>
+  );
+}
+
+function TopicEditor({ onSubmit }: { onSubmit: (topic: string) => void }) {
   const [topicDraft, setTopicDraft] = useState("");
   const [topicError, setTopicError] = useState("");
 
   return (
-    <>
-      {!topic && (
-        <>
-          <button
-            onClick={async () => {
-              const topic = createTopic();
-              await swarm.join(topic);
-              setTopic(topic);
-            }}
-          >
-            Start Chat
-          </button>
-          <button onClick={() => setShowTopicForm(true)}>Join Chat</button>
-          {showTopicFrom && (
-            <form
-              onSubmit={async (event) => {
-                event.preventDefault();
-                if (!isValidTopic(topicDraft)) {
-                  setTopicError("invalid topic");
-                } else {
-                  await swarm.join(topicDraft);
-                  setTopic(topicDraft);
-                }
-              }}
-            >
-              <label htmlFor="topic-input">Topic</label>
-              <input
-                id="topic-input"
-                autoFocus
-                value={topicDraft}
-                onChange={(event) => {
-                  setTopicDraft(event.target.value);
-                }}
-              />
-              {Boolean(topicError) && <p>{topicError}</p>}
-            </form>
-          )}
-        </>
-      )}
-      <p>{topic}</p>
-      {topic && (
-        <form
-          onSubmit={(event) => {
-            event.preventDefault();
-            swarm.sendAll(message);
-          }}
-        >
-          <label htmlFor="message-input">Message</label>
-          <input
-            id="message-input"
-            value={message}
-            onChange={(event) => {
-              setMessage(event.target.value);
-            }}
-          />
-        </form>
-      )}
-    </>
+    <form
+      onSubmit={async (event) => {
+        event.preventDefault();
+        if (!isValidTopic(topicDraft)) {
+          setTopicError("invalid topic");
+        } else {
+          onSubmit(topicDraft);
+        }
+      }}
+    >
+      <label htmlFor="topic-input">Topic</label>
+      <input
+        id="topic-input"
+        autoFocus
+        value={topicDraft}
+        onChange={(event) => {
+          setTopicDraft(event.target.value);
+        }}
+      />
+      {Boolean(topicError) && <p>{topicError}</p>}
+    </form>
+  );
+}
+
+function MessageEditor({ onSubmit }: { onSubmit: (message: string) => void }) {
+  const [message, setMessage] = useState("");
+
+  return (
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        onSubmit(message);
+      }}
+    >
+      <label htmlFor="message-input">Message</label>
+      <input
+        id="message-input"
+        value={message}
+        onChange={(event) => {
+          setMessage(event.target.value);
+        }}
+      />
+    </form>
   );
 }
