026995639ba6323d362e279852472764a1cd0e99
John Veresk <john.veresk@proton.me>
2024-11-03 12:15:53 +0200

    better npm build script

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
 export const App = () => (
-  <div className="bg-slate-400">Hello world! The beggining is here!</div>
+  <div className="bg-slate-400">Hello world! The beggining is here!!</div>
 )
 
